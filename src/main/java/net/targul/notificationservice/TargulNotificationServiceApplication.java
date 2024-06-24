d17a74c1d8182563442e8a5875e1b8e284e5a52a
SerhiiRybak <sergey24rybak@gmail.com>
2024-06-24 21:18:52 +0300

    Initial commit

---
+++ src/main/java/net/targul/notificationservice/TargulNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package net.targul.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargulNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TargulNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/targul/notificationservice/TargulNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.targul.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargulNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
